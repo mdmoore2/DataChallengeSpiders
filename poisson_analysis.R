60ef5ffc6bfb16726f14dd161bf62ce983f91165
Matthew Moore <mdmoore2@uchicago.edu>
2019-09-10 17:15:27 -0400

    normalize and bug fix plot 1

--- poisson_analysis.R
+++ poisson_analysis.R
@@ -18,18 +18,18 @@ mitchell <- read_csv('data/mitchell_weevil_egg_data_1975.csv')
 MeanSpider <- poison_mle(cole$k_number_of_arthropods, cole$C_count_of_boards_with_k_spiders)
 
 #create a varaible that represents the poisson distribution
-poisson_spiders <- dpois(cole$C_count_of_boards_with_k_spiders, MeanSpider, log=FALSE)
+poisson_spiders <- dpois(cole$k_number_of_arthropods, MeanSpider, log=FALSE)
 
 #adding dist to cole data frame
 cole <- cbind(cole, poisson_spiders)
 
 #plot the two plots on top of eachother using ggplot
 spider_plot <- ggplot(cole) + 
-  aes(x=k_number_of_arthropods, y=C_count_of_boards_with_k_spiders) + 
+  aes(x=k_number_of_arthropods, y=C_count_of_boards_with_k_spiders/sum(C_count_of_boards_with_k_spiders)) + 
   geom_point(colour='blue') +
   theme_minimal() +
   xlab('Number seen') +
-  ylab('Occurences')
+  ylab('Probability')
 
 spider_plot <- spider_plot + 
   geom_point(y=cole$poisson_spiders, x=cole$k_number_of_arthropods, colour='red')
