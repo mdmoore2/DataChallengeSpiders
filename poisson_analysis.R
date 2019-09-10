628cd24a9f1f5bfaf0378188c5083418a7972836
Jacob Drucker <jrdrucker@uchicago.edu>
2019-09-10 16:34:05 -0500

    refactored code

--- poisson_analysis.R
+++ poisson_analysis.R
@@ -2,6 +2,10 @@ library(tidyverse)
 library(ggplot2)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+#bring up the data as file-specific dataframes
+cole <- read_csv('data/cole_arthropod_data_1946.csv')
+mitchell <- read_csv('data/mitchell_weevil_egg_data_1975.csv')
+
 #function to correct occurence data to give proper average of bug occerence
 poison_mle<- function(occurence_data, weight_data){
   lambda = 0
@@ -10,30 +14,24 @@ poison_mle<- function(occurence_data, weight_data){
   }
   lambda <- lambda/ sum(occurence_data)
 }
-#bring up the data as file-specific dataframes
-cole <- read_csv('data/cole_arthropod_data_1946.csv')
-mitchell <- read_csv('data/mitchell_weevil_egg_data_1975.csv')
 
-#calculate the mean from a given column
-MeanSpider <- poison_mle(cole$k_number_of_arthropods, cole$C_count_of_boards_with_k_spiders)
-
-#create a varaible that represents the poisson distribution
-poisson_spiders <- dpois(cole$k_number_of_arthropods, MeanSpider, log=FALSE)
-
-#adding dist to cole data frame
-cole <- cbind(cole, poisson_spiders)
-
-#plot the two plots on top of eachother using ggplot
-spider_plot <- ggplot(cole) + 
-  aes(x=k_number_of_arthropods, y=C_count_of_boards_with_k_spiders/sum(C_count_of_boards_with_k_spiders)) + 
-  geom_point(colour='blue') +
-  theme_minimal() +
-  xlab('Number seen') +
-  ylab('Probability')
-
-spider_plot <- spider_plot + 
-  geom_point(y=cole$poisson_spiders, x=cole$k_number_of_arthropods, colour='red')
+plot_poisson_comp <- function(full_data,occurence_data,weight_data){
+  lambda <- poison_mle(occurence_data, weight_data) #calculate poisson_mle
+  poisson_data <- dpois(occurence_data, lambda, log=FALSE) #create a varaible that represents the poisson distribution
   
-show(spider_plot)
+  #plot the two plots on top of eachother using ggplot
+  plot_data <- ggplot(full_data) + 
+    aes(x=occurence_data, y=weight_data/sum(weight_data)) + 
+    geom_point(colour='blue') +
+    theme_minimal() +
+    xlab('Number seen') +
+    ylab('Probability')
+  
+  plot_data <- plot_data + 
+    geom_point(y=poisson_data, x=occurence_data, colour='red')
+  show(plot_data)
+}
 
-
+plot_poisson_comp(cole,cole$k_number_of_arthropods, cole$C_count_of_boards_with_k_spiders)
+plot_poisson_comp(cole,cole$k_number_of_arthropods, cole$C_count_of_boards_with_k_sowbugs)
+plot_poisson_comp(mitchell,mitchell$k_number_of_eggs,mitchell$C_count_of_beans_with_k_eggs)
