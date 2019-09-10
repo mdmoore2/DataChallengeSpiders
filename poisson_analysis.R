f16556b920aa70c59601d6a94bf7144766f622a4
Sean Ryan <seanr1@uchicago.edu>
2019-09-10 16:09:38 -0500

    Fixed bug with correct mean values

--- poisson_analysis.R
+++ poisson_analysis.R
@@ -2,12 +2,20 @@ library(tidyverse)
 library(ggplot2)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+#function to correct occurence data to give proper average of bug occerence
+poison_mle<- function(occurence_data, weight_data){
+  lambda = 0
+  for (i in 1:length(occurence_data)){
+    lambda <- lambda + occurence_data[i]*weight_data[i]
+  }
+  lambda <- lambda/ sum(occurence_data)
+}
 #bring up the data as file-specific dataframes
 cole <- read_csv('data/cole_arthropod_data_1946.csv')
 mitchell <- read_csv('data/mitchell_weevil_egg_data_1975.csv')
 
 #calculate the mean from a given column
-MeanSpider <- mean(cole$C_count_of_boards_with_k_spiders)
+MeanSpider <- poison_mle(cole$k_number_of_arthropods, cole$C_count_of_boards_with_k_spiders)
 
 #create a varaible that represents the poisson distribution
 poisson_spiders <- dpois(cole$C_count_of_boards_with_k_spiders, MeanSpider, log=FALSE)
@@ -26,6 +34,6 @@ spider_plot <- ggplot(cole) +
 spider_plot <- spider_plot + 
   geom_point(y=cole$poisson_spiders, x=cole$k_number_of_arthropods, colour='red')
   
-
+show(spider_plot)
 
 
