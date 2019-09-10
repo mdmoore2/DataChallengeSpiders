ae39c4e764c26cffed0d5846cd81560b397fff8f
Anqi Yu <aqyu@uchicago.edu>
2019-09-10 17:25:31 -0400

    Add ggplot

--- Spider_Anqi_Sylvi_Jacob_part1 copy.R
+++ Spider_Anqi_Sylvi_Jacob_part1 copy.R
@@ -12,3 +12,13 @@ lambda_weevil <- mean(numWeevil)
 
 norm_number_spider <- arthropod$C_count_of_boards_with_k_spiders/sum(numSpider)
 plot(arthropod$k_number_of_arthropods, norm_number_spider)
+
+poisSpider <- dpois(arthropod$k_number_of_arthropods, lambda_spiders, log = FALSE)
+plot(poisSpider) 
+
+spider <- cbind((0:17), numSpider, poisSpider, norm_number_spider)
+spider <- as.data.frame(spider)
+colnames(spider) <- ("num_arthropod", "numSpider", "poisSpider", "norm_number_spider")
+ggplot(data = spider) + 
+  geom_smooth(mapping = aes(x = num_arthropod, y = poisSpider))+
+  geom_point(mapping = aes(x = num_arthropod, y = norm_number_spider))
